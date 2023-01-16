8cb7a112e357d62559086d51ab0ca91dd43f3576
Gameplay07HUN <gameplay719@gmail.com>
2023-01-16 14:40:44 +0100

    Add project files.

---
+++ WindowsFormsAppButtonPicture/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsAppButtonPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KepCuccPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // KepCuccPanel
+            // 
+            this.KepCuccPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.KepCuccPanel.Location = new System.Drawing.Point(0, 0);
+            this.KepCuccPanel.Name = "KepCuccPanel";
+            this.KepCuccPanel.Size = new System.Drawing.Size(800, 450);
+            this.KepCuccPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.KepCuccPanel);
+            this.Name = "Form1";
+            this.Text = "Parancsgomb hátterének a megváltoztatása";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel KepCuccPanel;
+    }
+}
+
---
+++ WindowsFormsAppButtonPicture/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppButtonPicture
+{
+    public partial class Form1 : Form
+    {
+        int sorokSzama = 30;
+        int oszlopokSzema = 60;
+        int imageSize = 300;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < oszlopokSzema; j++)
+                {
+                    PictureBox picture = new PictureBox();
+                    picture.SetBounds(j * imageSize, i * imageSize, imageSize, imageSize);
+                    picture.Text = (i * oszlopokSzema + j).ToString();
+                    picture.Click += (o, ev) =>
+                    {
+                        PictureBox kep = (PictureBox)o;
+                        kep.Image =(Properties.Resources.bottom_gear);
+                        kep.SizeMode=PictureBoxSizeMode.StretchImage;
+                    };
+                    KepCuccPanel.Controls.Add(picture);
+                }
+            }
+        }
+    }
+}
